c95dc31521a0c1760ccaaac7c388812eadb232dc
Adri <aakeem0920@gmail.com>
2025-10-15 19:00:40 -0400

    Adding git reset tool

--- pull.sh
+++ pull.sh
@@ -2,16 +2,16 @@
 
 # Check if a path argument is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 /path/to/git/repo"
-  exit 1
+    echo "Usage: $0 /path/to/git/repo"
+    exit 1
 fi
 
 REPO_PATH="$1"
 
 # Check if the path is a valid Git repository
 if [ ! -d "$REPO_PATH/.git" ]; then
-  echo "Error: '$REPO_PATH' is not a valid Git repository."
-  exit 1
+    echo "Error: '$REPO_PATH' is not a valid Git repository."
+    exit 1
 fi
 
 # Move into the repo path
